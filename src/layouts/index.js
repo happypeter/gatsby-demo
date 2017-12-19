766b57e9788c7113d57199e5b59a96de0257ca48
Peter Wang <happypeter1983@gmail.com>
2017-12-19 22:56:04 +0800

    gatsby-link

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Link from 'gatsby-link'
 
 export default ({ children }) => {
   return (
     <div>
-      LAYOUT
+      <Link to='/'>首页</Link> &nbsp;
+      <Link to='/about'>关于</Link>
       {children()}
     </div>
   )
